a466c0a651df2f0ed23039370cb22c5b44e29a83
Karthi-dev123 <karthikeyadevaraj@gmail.com>
2026-02-12 10:56:34 +0530

    UC1: Corrected Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,14 @@
+public class PalindromeCheckerApp {
+
+    //public class UseCase1PalindromeCheckerApp{
+
+
+
+        public static void main(String[] args){
+            // print welcome message here
+            System.out.println("Welcome to the Palindrome Checker Management System");
+            System.out.println("Version : 1.0");
+            System.out.println("System initialized Successfully,");
+        }
+   // }
+}
